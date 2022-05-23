4bb58331e181f8261f865d15a3c7d9a5447127f0
Francisco Lima Figueiredo (Chicão) <francisco.figueiredo@caixa.gov.br>
2022-05-23 00:17:59 -0300

    Chico: site buildado e posto no ar

--- Publish/PRD-gecdi.mz.caixa_conheca-gerenciador.caixa-WITH-BUILD.bat
+++ Publish/PRD-gecdi.mz.caixa_conheca-gerenciador.caixa-WITH-BUILD.bat
@@ -1,11 +1,13 @@
 cd ..\FrontEnd
-REM npm run build:prod
+npm run build:prod
 call ng build --configuration production --build-optimizer --optimization --progress --verbose --aot
 
 cd ..\PushAPI
-call dotnet build /p:DeployOnBuild=true /p:PublishProfile=Properties\PublishProfiles\Produção.pubxml
+
+call dotnet build /p:DeployOnBuild=true /p:PublishProfile="%CD%/Properties/PublishProfiles/Producao.pubxml"
 
 cd ..\Publish
+
 
 robocopy \\gecdi.mz.caixa\1048conheca-gerenciador.caixa\log\ .\PRD-gecdi.mz.caixa\log\ /s /eta /z /xd log /w:2 /r:100 /mt:8 /PURGE
 robocopy \\gecdi.mz.caixa\1048conheca-gerenciador.caixa\log\ .\PRD-gecdi.mz.caixa\log\ /s /eta /z /xd log /w:2 /r:100 /mt:8 /PURGE
